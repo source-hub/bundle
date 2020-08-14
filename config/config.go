ac4422c6cb9c85365c690230505928f51fd04db2
mahesh-9 <umamaheshnov9@gmail.com>
2020-08-13 21:24:30 -0500

    changed db name

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ func (c *Config) GetConfig() map[string]string {
 	}
 
 	db_env := map[string]string{
-		"sonu":   "host=postgres_12_container port=5432 user=sonu dbname=bundle password=code sslmode=disable",
+		"sonu_db":   "host=postgres_12_container port=5432 user=sonu dbname=bundle password=code sslmode=disable",
 	}
 
 	// should be moved to environment variables once the main checklist is done.
@@ -23,7 +23,7 @@ func (c *Config) GetConfig() map[string]string {
 		"foo":        "Bar",
 		"jwtKey":     "SomeRandomJWTKEY",
 		"db_dialect": "postgres",
-		"db_env":     db_env["sonu"],
+		"db_env":     db_env["sonu_db"],
 		"sendgrid":   "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",
 		"PORT":       port,
 	}
